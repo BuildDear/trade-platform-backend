21247e8be4dc6a881e6b67ebe183a9011cd8a10b
marik <letschuka@gmail.com>
2024-04-02 13:57:41 +0300

    update async db helper

--- src/config.py
+++ src/config.py
@@ -5,12 +5,12 @@ from pydantic_settings import BaseSettings
 BASE_DIR = Path(__file__).parent.parent
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
+
 class Setting(BaseSettings):
     api_v1_prefix: str = "/api/v1"
 
     db_url: str = SQLALCHEMY_DATABASE_URL
-    db_echo: bool = False
-    # db_echo: bool = True
+    db_echo: bool = True
 
 
 settings = Setting()
--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+
+
 app = FastAPI()
 
 

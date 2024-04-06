f77d7f1d32a4eb83c0be8043eb5db7ec8651bb3e
marik <letschuka@gmail.com>
2024-04-06 14:11:20 +0300

    install alembic

--- src/core/models/trader.py
+++ src/core/models/trader.py
@@ -3,12 +3,11 @@ from src.core.models.base_model import Base
 
 
 class Trader(Base):
-    __tablename__ = "user"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
     username: Mapped[str] = mapped_column(index=True, unique=True)
     email: Mapped[str] = mapped_column(index=True, unique=True)
     first_name: Mapped[str]
     last_name: Mapped[str]
     hashed_password: Mapped[str]
     is_superuser: Mapped[bool] = mapped_column(default=False)
+
--- src/core/utils/config.py
+++ src/core/utils/config.py
@@ -1,9 +1,12 @@
 import os
+from pathlib import Path
+
 from pydantic_settings import BaseSettings
+
+BASE_DIR = Path(__file__).parent.parent
 
 
 class Setting(BaseSettings):
-
     api_v1_prefix: str = "/api/v1"
     db_echo: bool = True
     db_url: str = os.getenv("DATABASE_URL")
